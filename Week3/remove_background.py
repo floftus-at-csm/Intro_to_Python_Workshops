b310cccc27e25434a855fff1db68a900ac9fa9ec
floftus <floftus@LAPTOP-QRTPH0JE.localdomain>
2022-11-29 22:31:53 +0000

    added video processing

---
+++ Week3/remove_background.py
@@ -0,0 +1,31 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture('Week3/content/input/Groovy Dancing Girl-Sr2JneittqQ.mp4')
+# out = cv2.VideoWriter('Week3/content/output.mp4', -1, 20.0, (640,480))
+# out = cv2.VideoWriter("output.mp4",cv2.VideoWriter_fourcc(*"MJPG"), 30,(640,480))
+ret, frame = cap.read()
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+fheight = frame.shape[0]
+fwidth = frame.shape[1]
+out = cv2.VideoWriter('output.avi',fourcc, 20.0, (fwidth,fheight))
+while True:
+    ret, frame = cap.read()
+    if ret == True:
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        lower_blue = np.array([0, 0, 120])
+        upper_blue = np.array([180, 38, 255])
+        mask = cv2.inRange(hsv, lower_blue, upper_blue)
+        result = cv2.bitwise_and(frame, frame, mask=mask)
+        b, g, r = cv2.split(result)  
+        filter = g.copy()
+        ret,mask = cv2.threshold(filter,10,255, 1)
+        frame[ mask == 255] = 0
+        # cv2.imshow('frame',frame)
+        out.write(frame)
+    else:
+        break
+# Release everything if job is finished
+cap.release()
+out.release()
+cv2.destroyAllWindows()
---
+++ Week3/session3.md
@@ -0,0 +1,48 @@
+## recap
+
+## Data Structures
+### Arrays
+Lists and arrays are one of the core data structures in python. For now, lets see them as interchangeable terms.
+Lists allow you to group data together. 
+We declare an empty list like
+```
+my_list = []
+```
+or a list with content like
+```
+my_list = ["youtube_url", "youtube_url2"]
+```
+or an empty list of a specific size
+```
+my_empty_list = [None] * 10
+```
+
+
+* arrays/lists
+    * creating them
+    * accesing values
+    * deletings values
+* get file paths in folder into l
+
+## loops
+
+### For Loops
+* showing with the console
+doing all files in a folder
+* standard for loops over arrays
+* standard for loops (range)
+* for loops with enumerate
+
+### While Loops
+* for loops
+* show break
+* exiting via keyboard pressed 
+
+## functions
+* same function as before
+* add in a new process
+* return something
+
+
+
+web cam
