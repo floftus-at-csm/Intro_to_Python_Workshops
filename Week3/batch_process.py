b6eb08c4eff48d88115012732ed5121d0aefa51f
Felix Loftus <floftus.csm@gmail.com>
2023-01-25 13:40:47 +0000

    week4

--- Week3/batch_process.py
+++ Week3/batch_process.py
@@ -1,8 +1,26 @@
 from PIL import Image, ImageOps
 from pathlib import Path
 
-# if __name__ == "__main__":
-input_folder = Path("content/input_images")
+def convert_image_to_jpg(img):
+    # This function converts a png image to a jpg
+    new_img = Image.new("RGB", img.size, (255, 255, 255))
+    new_img.paste(img, mask=img.split()[3]) # 3 is the alpha channel
+    return new_img
 
-directories = list(input_folder.iterdir())
-print(directories)
+if __name__ == "__main__":
+    input_folder = Path("content/input_images")
+    output_folder = Path("content/output_images")
+    directories = list(input_folder.iterdir())
+    print(directories)
+    for count, item in enumerate(directories):
+        im1 = Image.open(item) 
+        try:
+            im2 = ImageOps.solarize(im1, threshold = 130) 
+        except:
+            im1 = convert_image_to_jpg(im1)
+            im2 = ImageOps.solarize(im1, threshold = 130)
+            
+        # im2.show()
+        output_path = "content/output_images/" + str(count) + '.png'
+        im2.save(output_path)
+        print("image_saved")
--- Week3/example_code.py
+++ Week3/example_code.py
@@ -1,22 +1,24 @@
 # Creating an empty array
-empty_array = []
+# empty_array = [None] * 100
 
-empty_array.append(100) # adding a value to that array
-print(empty_array)
+# empty_array.append(100) # adding a value to that array
+# print(empty_array)
 
 # empty_array[3] = 1 # this will give an error because the third position does not exist
 # print(empty_array)
 
-empty_array = empty_array * 3 # making the array longer
-print(empty_array)
+# empty_array = empty_array * 3 # making the array longer
+# print(empty_array)
 
-genres = ["rock", "hip hop", "dance"]
-print(genres[0])
+genres = ["rock", "hip hop", "dance", "classical"]
+print(genres[2])
 genres[2] = "classical"
 print(genres[2])
 
-del genres[0]
-print(genres)
-genres.append("heavy metal")
+# del genres[0]
+# print(genres)
+# genres.append("heavy metal")
+print("the array before removing classical is: ", genres)
 genres.remove("classical")
-print(genres)
+print("the array after removing classical: ", genres )
+# print(genres)
